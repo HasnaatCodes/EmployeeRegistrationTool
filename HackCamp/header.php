75d5df3645ff95e5a899a4f7a7d88c6d026090c4
Akhtar Hasnaat <H.Akhtar8@edu.salford.ac.uk>
2020-01-16 14:49:29 +0000

    Sign Up page added (only for the admin)

--- HackCamp/header.php
+++ HackCamp/header.php
@@ -4,7 +4,7 @@ require_once 'Models/UserDataSet.php';
 session_start();
 
 $view = new stdClass();
-$view->pageTitle = 'Register';
+$view->pageTitle = 'Sign Up';
 $userDataSet = new UserDataSet();
 
 $view->employees = $userDataSet->fetchEmployees();
@@ -13,17 +13,10 @@ $view->employees = $userDataSet->fetchEmployees();
  *
  * * Send the values from the form to the createUser function in userDataSet and create a new User
  */
-if(isset($_POST['signup'])){
-    $firstname      = $_POST['firstname'];
-    $lastname       = $_POST['lastname'];
-    $email          = $_POST['email'];
-    $password       = $_POST['password'];
-    $userDataSet->createUser($firstname, $lastname, $email, $password);
-
-}
 
 if(isset($_SESSION['login'])){
     $view->email = $_SESSION['login']->getEmail();
 }
 
-require_once('Views/register.phtml');
+
+require_once('Views/template/newheader.phtml');
